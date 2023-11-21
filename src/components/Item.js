33fcae6b4f2d4659287e98f8a23e870c92d1214f
Maddie Weber <maddieweber7@gmail.com>
2023-11-21 15:34:51 -0500

    done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [cartStatus, setCartStatus] = useState(false)
+  const appClass2 = cartStatus ? "in-cart" : "";
+
+  function clickAddHandler() {
+    setCartStatus((prevCartStatus) => !prevCartStatus)
+  }
+
   return (
-    <li className="">
+    <li className={appClass2}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={clickAddHandler}>{cartStatus ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
